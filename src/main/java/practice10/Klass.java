2758e574dcf274f4bb0b96cbffee5abbc9e718f5
LiYanLance <leeyanly@163.com>
2018-04-03 22:03:33 +0800

    practice10Test all passed

--- src/main/java/practice10/Klass.java
+++ src/main/java/practice10/Klass.java
@@ -1,4 +1,55 @@
 package practice10;
 
 public class Klass {
+    private int number;
+    private Student leader;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void assignLeader(Student student) {
+        if (number == student.getKlass().getNumber()) {
+            leader = student;
+        } else {
+            System.out.print("It is not one of us.\n");
+        }
+    }
+
+    public void appendMember(Student student) {
+        student.getKlass().setNumber(this.number);
+    }
+
+    public boolean isIn(Student student) {
+        return this.equals(student.getKlass());
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj instanceof Klass){
+            return ((Klass) obj).getNumber() == number;
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return this.number;
+    }
+
+    public String getDisplayName() {
+        return "Class " + number;
+    }
 }
--- src/main/java/practice10/Teacher.java
+++ src/main/java/practice10/Teacher.java
@@ -1,4 +1,40 @@
 package practice10;
 
-public class Teacher {
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Teacher extends Person{
+
+    private Set<Klass> classes;
+
+    public Teacher(int id, String name, int age, Set<Klass> classes) {
+        super(id, name, age);
+        this.classes = classes;
+    }
+
+    public Teacher(int id, String name, int age){
+        super(id, name, age);
+        classes = new HashSet<>();
+    }
+
+    public boolean isTeaching(Student student) {
+        return classes.contains(student.getKlass());
+    }
+
+    @Override
+    public String introduce() {
+        String classesString = classes.stream().mapToInt(Klass::getNumber)
+                .mapToObj(n -> String.valueOf(n)).collect(Collectors.joining(", "));
+        String teacherIntroduce = !classes.isEmpty() ? "I teach Class " + classesString + "." : "I teach No Class.";
+        return super.introduce() + " I am a Teacher. " + teacherIntroduce;
+    }
+
+    public String introduceWith(Student student) {
+        String teacherTeach =  " I " +  (classes.contains(student.getKlass()) ? "" : "don't ") + "teach " + student.getName()  + ".";
+        return super.introduce() + " I am a Teacher." + teacherTeach;
+    }
+
+    public Set<Klass> getClasses(){
+        return classes;
+    }
 }
