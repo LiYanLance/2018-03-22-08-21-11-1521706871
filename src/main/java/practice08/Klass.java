e68cbe53ffd851371a52514f7c1aec7a7b33ccc5
LiYanLance <leeyanly@163.com>
2018-04-03 20:33:12 +0800

    practice08Test all passed

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,38 @@
 package practice08;
 
 public class Klass {
+    private int number;
+    private Student leader;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public void assignLeader(Student student) {
+        leader = student;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj instanceof Klass){
+            return ((Klass) obj).getNumber() == number;
+        }
+        return false;
+    }
+
+    public String getDisplayName() {
+        return "Class " + number;
+    }
 }
