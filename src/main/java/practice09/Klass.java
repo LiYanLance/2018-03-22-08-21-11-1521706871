2ebecfd12a4a471dcf363d45055d68cb75dcb536
LiYanLance <leeyanly@163.com>
2018-04-03 20:50:48 +0800

    practice09Test all passed

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,46 @@
 package practice09;
 
 public class Klass {
+    private int number;
+    private Student leader;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void assignLeader(Student student) {
+        if (number == student.getKlass().getNumber()) {
+            leader = student;
+        } else {
+            System.out.print("It is not one of us.\n");
+        }
+    }
+
+    public void appendMember(Student student) {
+        student.getKlass().setNumber(this.number);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj instanceof Klass){
+            return ((Klass) obj).getNumber() == number;
+        }
+        return false;
+    }
+
+    public String getDisplayName() {
+        return "Class " + number;
+    }
 }
--- src/main/java/practice09/Student.java
+++ src/main/java/practice09/Student.java
@@ -1,4 +1,23 @@
 package practice09;
 
-public class Student {
+public class Student extends Person{
+
+    private Klass klass;
+
+    public Student(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+    public String introduce() {
+        return super.introduce() +  " I am a Student. I am " + (this.equals(klass.getLeader()) ? "Leader of " : "at ") + klass.getDisplayName() + ".";
+    }
 }
