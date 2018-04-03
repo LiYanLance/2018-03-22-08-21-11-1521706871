93af9947a65655c9eecebe76fc4904cecb9198d0
LiYanLance <leeyanly@163.com>
2018-04-03 19:45:29 +0800

    practice06Test all passed

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,30 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+
+    private int klass;
+
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age){
+        super(name, age);
+        klass = -1;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        String teacherIntoduce = klass != -1 ? "I teach Class " + klass +"." : "I teach No Class.";
+        return super.introduce() + " I am a Teacher. " + teacherIntoduce;
+    }
 }
