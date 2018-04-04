dea7cf137106b22938143d0d2ce7586ab2291974
LiYanLance <leeyanly@163.com>
2018-04-04 10:32:09 +0800

    BUILD SUCCESSFUL (Practice11Test all passed)

--- src/main/java/practice11/Klass.java
+++ src/main/java/practice11/Klass.java
@@ -1,4 +1,72 @@
 package practice11;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Klass {
+    private int number;
+    private Student leader;
+    private Set<Teacher> teachers = new HashSet<>();
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void assignLeader(Student student) {
+        if (number == student.getKlass().getNumber()) {
+            leader = student;
+            notifyTeacher(student.getName() + " become Leader of Class " + number + ".\n");
+        } else {
+            System.out.print("It is not one of us.\n");
+        }
+    }
+
+    public void appendMember(Student student) {
+        student.getKlass().setNumber(this.number);
+        notifyTeacher(student.getName() + " has joined Class " + number + ".\n");
+    }
+
+    public boolean isIn(Student student) {
+        return this.equals(student.getKlass());
+    }
+
+    public String getDisplayName() {
+        return "Class " + number;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Klass) {
+            return ((Klass) obj).getNumber() == number;
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return this.number;
+    }
+
+    public void addTeacher(Teacher teacher) {
+        teachers.add(teacher);
+    }
+
+    public void notifyTeacher(String message) {
+        if(!teachers.isEmpty()){
+            teachers.forEach(teacher -> teacher.reciveMessage(message));
+        }
+    }
+
 }
--- src/main/java/practice11/Teacher.java
+++ src/main/java/practice11/Teacher.java
@@ -1,4 +1,51 @@
 package practice11;
 
-public class Teacher {
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class Teacher extends Person{
+
+    private Set<Klass> classes;
+
+    public Teacher(int id, String name, int age, Set<Klass> classes) {
+        super(id, name, age);
+        this.classes = classes;
+        classes.forEach(klass -> klass.addTeacher(this));
+    }
+
+    public Teacher(int id, String name, int age){
+        super(id, name, age);
+        classes = new HashSet<>();
+    }
+
+    public boolean isTeaching(Student student) {
+        for (Klass klass : classes) {
+            if (klass.isIn(student)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public String introduce() {
+        String classesString = classes.stream().mapToInt(Klass::getNumber)
+                .mapToObj(n -> String.valueOf(n)).collect(Collectors.joining(", "));
+        String teacherIntroduce = !classes.isEmpty() ? "I teach Class " + classesString + "." : "I teach No Class.";
+        return super.introduce() + " I am a Teacher. " + teacherIntroduce;
+    }
+
+    public String introduceWith(Student student) {
+        String teacherTeach =  " I " +  (classes.contains(student.getKlass()) ? "" : "don't ") + "teach " + student.getName()  + ".";
+        return super.introduce() + " I am a Teacher." + teacherTeach;
+    }
+
+    public Set<Klass> getClasses(){
+        return classes;
+    }
+
+    public void reciveMessage(String message) {
+        System.out.print("I am " + getName() + ". I know " + message);
+    }
 }
